8c32c861cc5904e070489f643c2ce5f25b0ae784
vfuster66 <124174795+vfuster66@users.noreply.github.com>
2023-12-26 11:59:20 +0100

    Update HumanA.cpp

--- CPP_module_01/ex03/HumanA.cpp
+++ CPP_module_01/ex03/HumanA.cpp
@@ -16,6 +16,13 @@
  *                                 CONSTRUCTEUR                              *
 *****************************************************************************/
 
+// Initialisation d'un objet HumanA qui prend 2 parametres :
+// name		-> une reference constante a une std::string
+// weapon 	-> une reference a un objet Weapon
+// Liste d'initialisation pour assigner name a _name et weapon a _weapon
+// Cela permet que chaque objet HumanA aura un nom et utilisera l'arme
+// passee au constructeur.
+
 HumanA::HumanA(const std::string &name, Weapon &weapon) : _name(name), _weapon(weapon) {}
 
 /*****************************************************************************
@@ -27,6 +34,9 @@ HumanA::~HumanA() {}
 /*****************************************************************************
  *                                 FONCTIONS                                 *
 *****************************************************************************/
+
+// Methode utilisee pour simuler une attaque par l'objet HumanA
+// const	-> ne modifie pas l'etat de l'objet HumanA
 
 void HumanA::attack() const {
 	std::cout << _name << " attacks with their " << _weapon.getType() << std::endl;
