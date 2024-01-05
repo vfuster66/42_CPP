40e8c9fa240ef3b9555e5f4a896f61e44ba4b77f
vfuster66 <124174795+vfuster66@users.noreply.github.com>
2024-01-05 17:08:14 +0100

    Add files via upload

---
+++ CPP_module_07/ex00/Whatever.hpp
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vfuster- <vfuster-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/05 17:04:38 by vfuster-          #+#    #+#             */
+/*   Updated: 2024/01/05 17:07:06 by vfuster-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+#include <iostream>
+
+template <typename T>
+
+void swap(T &a, T &b)
+{
+    T temp = a;
+    a = b;
+    b = temp;
+}
+
+template <typename T>
+T min(const T &a, const T &b)
+{
+    if (a < b)
+	{
+        return a;
+    }
+	else
+	{
+        return b;
+    }
+}
+
+template <typename T>
+
+T max(const T &a, const T &b)
+{
+    if (a > b)
+	{
+        return a;
+    }
+	else
+	{
+        return b;
+    }
+}
+
+#endif
