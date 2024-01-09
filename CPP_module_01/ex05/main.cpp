82b02d82dc2dc3737254b6850a44aa6c2d5e536a
vfuster66 <124174795+vfuster66@users.noreply.github.com>
2024-01-09 14:17:20 +0100

    Add files via upload

--- CPP_module_01/ex05/main.cpp
+++ CPP_module_01/ex05/main.cpp
@@ -6,7 +6,7 @@
 /*   By: vfuster- <vfuster-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/21 15:55:28 by vfuster-          #+#    #+#             */
-/*   Updated: 2023/12/26 15:15:08 by vfuster-         ###   ########.fr       */
+/*   Updated: 2023/12/28 14:53:01 by vfuster-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,6 @@ int main()
 {
 	Harl harl;
 	
-	std::cout << "[Testing with valid levels]" << std::endl;
-    
 	harl.complain("DEBUG");
 	harl.complain("INFO");
 	harl.complain("WARNING");
